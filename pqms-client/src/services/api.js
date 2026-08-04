1e8df3873d474ae73189453919bb8ea294fa01c7
metehancihangir <metehancihangir10@gmail.com>
2026-08-04 21:34:15 +0300

    firebase add

--- pqms-client/src/services/api.js
+++ pqms-client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5140/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5140/api',
 });
 
 // Her istekte JWT token'ı header'a ekle
